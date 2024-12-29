760fa3580e6d617e7c634141184ce0fbd8899e1a
Dang Dizi <dangdizi.com@gmail.com>
2024-12-29 09:31:59 +0700

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,25 @@ import { createTodo } from "@/actions/todoAction";
 import { Button } from "@/components/button/Button";
 import Form from "@/components/form/Form";
 import Input from "@/components/input/Input";
+import { prisma } from "@/utils/prisma";
 
-export default function Home() {
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted: true,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div>
       <h1 className="text-4xl font-extrabold">Đăng Dizi</h1>
@@ -11,7 +28,11 @@ export default function Home() {
         <Input name="input" type="text" placeholder="enter todo..."></Input>
         <Button text="add todo" type="submit"></Button>
       </Form>
+      {
+        data.map((todo, id) => (
+          <div key={id}>{todo.title}</div>
+        ))
+      }
     </div>
-
   );
 }
